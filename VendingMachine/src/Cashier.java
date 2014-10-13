cf1ea2cce5ccc614d3e51c1020f6ba76907e666c
nathan.mcguire <nathan.mcguire@MU-C3-18.mullen.pvt.k12.co.us>
2014-10-13 13:37:12 -0600

    Testing Git Hub

---
+++ VendingMachine/src/Cashier.java
@@ -0,0 +1,45 @@
+//handle money, return change 
+public class Cashier
+	{
+	private int currentCents;//amount of change the user inputs
+	
+	public int addMoney()//adds value to vending machine
+		{
+	
+		return;
+		}
+	
+	public int currentValue()//converts money given into size 2 array
+		{
+	
+		return;
+		}
+	
+	public void purchase()//does math with given amount and price
+		{
+
+		}
+	
+	public String returnChange()//returns change
+		{
+		
+		return;
+		}
+	
+	public static String showCash()//returns amount of money input into the machine
+		{
+		
+		return;
+		}
+	
+	public static int toPennies()//converts size 2 array into money
+		{
+
+		return;
+		}
+	
+	public static void runAllCashierMethods()//runs all methods in this class
+		{
+		
+		}
+	}
---
+++ VendingMachine/src/VendingMachine.java
@@ -0,0 +1,50 @@
+//handles input from user
+//sets up vending machine products
+import java.util.Scanner;
+
+public class VendingMachine
+	{
+	private Cashier cashier;
+	private InventoryManager im;
+	private Scanner keypad;
+	
+	public static void main(java.lang.String[] args)//main method
+		{
+		runAllVendingMachineMethods();
+		Cashier.runAllCashierMethods();
+		}
+	
+	public void alerUser()//Prints out user inputs to this machine
+		{
+		
+		}
+	
+	public void initProducts()//hard code snacks
+		{	
+	
+		}
+	
+	public void listen()//infinite loop that listens for keypad input
+		{
+	
+		}
+	
+	public void turnOn()//initializes products into the vending machine
+		{
+	
+		}
+
+	public static void runAllVendingMachineMethods()//runs all methods in this class
+		{
+		
+		}
+	}
+//1 User inserts money only coins
+//User enters selection(2 digit)
+//Is the number valid(if statement)
+//Is the item in stock(if statement)
+//Did the user insert enough $(if statement)
+//Machine releases item
+//Machine returns best change
+//
+
