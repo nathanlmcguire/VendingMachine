dbf6d29ce93bd134fe278eca4ad006797bdedeb7
nathan.mcguire <nathan.mcguire@MU-C3-18.mullen.pvt.k12.co.us>
2014-10-14 13:33:39 -0600

    Made huge progress on cashier and vendimg machine

---
+++ VendingMachine/VendingMachine.java
@@ -0,0 +1,58 @@
+//handles input from user
+//sets up vending machine products
+import java.util.Scanner;
+
+public class VendingMachine
+	{
+	private Cashier cashier;
+	private InventoryManager im;
+	private Scanner keypad;
+	String itemChoice;
+	
+	public static void main(java.lang.String[] args)//main method
+		{
+		//make vending machine obj
+		String <VendingMachine> VendingMachineObject = new String<VendingMachine>();
+		turnOn();
+		listen();
+		}
+	
+	public void alerUser()//Prints out user inputs to this machine
+		{
+		//cashier 
+		initProducts();
+		Cashier.addMoney();
+		Cashier.purchase();
+		//offers to let user not buy
+		}
+	
+	public void initProducts()//give user choice of snack
+		{	
+		System.out.println("What product would you like?");
+		for(int i = 0; i < 10; i++)
+		System.out.println("----------------------------------------------------------------------");
+		System.out.println(ProductManager.listProducts.get(i).getItemCode() + " = " + Product.stock.get(i).getSnackName() + ", " + Product.stock.get(i).getCostInCents() + " cents, " + ProductManager.listProducts.get(i).getQuantity() + "q");
+		System.out.println("----------------------------------------------------------------------");
+		
+		}
+	
+	public void listen()//infinite loop that listens for keypad input
+		{
+		alertUser();
+		}
+	
+	public void turnOn()//initializes products into the vending machine
+		{
+		//Product, inventory manager, and product manager methods go here
+		}
+	
+	}
+//1 User inserts money only coins
+//User enters selection(2 digit)
+//Is the number valid(if statement)
+//Is the item in stock(if statement)
+//Did the user insert enough $(if statement)
+//Machine releases item
+//Machine returns best change
+//
+
--- VendingMachine/src/Cashier.java
+++ VendingMachine/src/Cashier.java
@@ -1,45 +1,102 @@
+import java.util.Scanner;
+
 //handle money, return change 
 public class Cashier
 	{
 	private int currentCents;//amount of change the user inputs
+	int changeOfTransaction, condition = 0;//change from transaction and while loop helper
+	String coinName;
+	
 	
 	public int addMoney()//adds value to vending machine
 		{
-	
-		return;
+		while(condition < 1)
+			{
+			System.out.println("What coins will you insert?  Type 'done' to stop.");
+			Scanner userInput1 = new Scanner(System.in);		
+			coinName = userInput1.nextLine();	
+			currentValue();		
+			}
+		showCash();
+		return currentCents;
 		}
 	
-	public int currentValue()//converts money given into size 2 array
+	public int currentValue()//converts money given into size 2 array DONE
 		{
-	
-		return;
+		switch(coinName)
+			{
+			case "nickel":
+				{
+				currentCents = currentCents + 5;
+				break;
+				}
+				
+			case "penny":
+				{
+				currentCents = currentCents + 1;
+				break;
+				}
+				
+			case "quarter":
+				{
+				currentCents = currentCents + 25;
+				break;
+				}
+				
+			case "dime":
+				{
+				currentCents = currentCents + 10;
+				break;
+				}
+				
+			case "done":
+				{
+				condition = 1;
+				break;
+				}
+			
+			default:
+				{
+				System.out.println("SYSTEM ERROR!!!!!");
+				break;
+				}
+			}
+		return currentCents;
 		}
 	
-	public void purchase()//does math with given amount and price
+	public void purchase()//does math with given amount and price DONE
 		{
-
+		if(Products.costInCents <= currentCents)
+			{
+			changeOfTransaction = Product.costInCents - currentCents;	
+			//toPennies();
+			}
+		else
+			{
+			System.out.println("You do not have enough money to purchase this item.");
+			}
+		returnChange();
 		}
 	
-	public String returnChange()//returns change
-		{
-		
-		return;
+	public String returnChange()//returns change HELP
+		{		
+		String change = changeOfTransaction + "cents";
+		System.out.println("You receive " + changeOfTransaction + " cents in change.");
+		return change;
 		}
 	
-	public static String showCash()//returns amount of money input into the machine
+	public /* static? */ String showCash()//returns amount of money input into the machine HELP
 		{
-		
-		return;
+		String cash = currentCents + "cents";
+		System.out.println("You entered " + currentCents + " into the vending machine.");
+		return cash;
 		}
 	
-	public static int toPennies()//converts size 2 array into money
-		{
-
-		return;
-		}
+//	public static int toPennies()//converts size 2 array into money
+//		{
+//		
+//		System.out.println("Your change is " + changeOfTransaction + " pennies as change.");		
+//		return;
+//		}
 	
-	public static void runAllCashierMethods()//runs all methods in this class
-		{
-		
-		}
 	}
