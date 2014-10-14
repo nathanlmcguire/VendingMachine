d5811ff10910118b09f22a20dcae24196a6a4935
chris.falkenberg <chris.falkenberg@MU-C3-17.mullen.pvt.k12.co.us>
2014-10-14 13:41:26 -0600

    Chris commit

--- VendingMachine/src/InventoryManager.java
+++ VendingMachine/src/InventoryManager.java
@@ -2,39 +2,67 @@ import java.util.ArrayList;
 public class InventoryManager
 extends java.lang.Object
 	{
+	/*
+	 * return all the ProductManager objects the InventoryManager knows about
+	 */
+	static ArrayList <ProductManager> listProducts= new ArrayList <ProductManager>();
 	
+	/*
+	 * will create an ArrayList of product objects where all the 
+	 * products in the arraylist have a price equal to or less than
+	 * the paramater passed in.
+	 */
+	ArrayList <Product> productsByPrice = new ArrayList <Product>();
 
-	public static void main(String[] args)
+
+	/*
+	 * runs all the methods in the InventoryManager class.
+	 */
+	public static void runAllInventoryManagerMethods()
 		{
 		
-
 		}
-	public static void addProduct()//make an Arraylist from Chris
+	/*
+	 * adds a product manager to the arraylist using the given params
+	 */
+	public static void addProduct()
 		{
-	
+		listProducts.add(new ProductManager(product, quantity));
 		}
-	public boolean attemptPurchase()//make sure input is valid-get a list of products-check if user can afford product-if user can afford it "vend" the item and return "true"-else return false
-		{
-			return false;
-		}
-	//Productmanager
+	/*
+	 * makes sure that the itemCode passed in is a valid item code
+	 * and that the item code is in stock. After those checks it 
+	 * gets a list of products taht are equal to or less than the
+	 * change currently in the machine.
+	 */
+	public boolean attemptPurchase()
 		{
 		
 		}
-	public boolean isInStock()// returns if product in stock(if statement)
+	/*
+	 * goes throught the list of ProductManagers and checks if
+	 * the item code past in exists.
+	 */
+	public ProductManager find()
 		{
-			return "";
+		
 		}
-	public boolean isValidItemCode()//determines is user input for code is valid(if statement)
+	/*
+	 * returns weather the machine has any of this product
+	 * in stock
+	 */
+	public boolean isInSock()
 		{
-		return"";
+		
 		}
-	//ArrayList<ProductManager>
-	//ArrayList<Product>
-	public static void runAllInventoryManager()//all my methods
-	{
+	/*
+	 * checks to make sure that the user inputted itemCode is
+	 * legit.
+	 */
+	public boolean isValidItemCode()
+		{
+		
+		}
 	
 	}
 	
-
-	}
